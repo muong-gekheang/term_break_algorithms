992d6b471c84fb701edfa922ccf833fed1f406c8
Muong Gek Heang <muonggekheang05@gmail.com>
2024-08-28 13:15:30 +0700

    git commit -m "Testing on how to commit and push"

--- Algorithms/pointer/number_swapping.cpp
+++ Algorithms/pointer/number_swapping.cpp
@@ -11,12 +11,14 @@ void swap (int *num1, int *num2)
 }
 int main()
 {
-    int num1 = 10;
-    int num2 = 20;
+    int num1 = 20;
+    int num2 = 30;
     int *pointer_of_num_1 = &num1;
     int *pointer_of_num_2 = &num2;
     cout << "before swapping the number are " << num1 << " and " << num2 << endl;
     swap(pointer_of_num_1, pointer_of_num_2);
+
+    cout << "new text is here, new changed";
 
 
 }
